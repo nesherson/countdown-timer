b896f239256ec0377629fbf1cbdccd3df2052acd
unknown <nesad.omeragich27@gmail.com>
2021-06-07 00:04:01 +0200

    Adds initial styling for sidebar

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,8 +1,58 @@
+import Styled from 'styled-components';
+
+const Aside = Styled.aside`
+    box-sizing: border-box;
+    width: 20%;
+    min-width: 220px;
+    background-color: #2e48cd;
+    color: #f4f4f4;
+    //padding: 35px 15px 20px 5%;
+    position: fixed;
+    height: 100%;
+    transition: background-color 0.2s ease;
+    @media (max-width: 768px) {
+        display: none;
+    }
+`;
+
+const Logo = Styled.div`
+    background-color:#596ed9;
+    color: #f4f4f4;
+    padding: 15px 20px;
+`;
+
+const NavigationList = Styled.ul`
+    list-style: none;
+    margin: 0;
+    padding: 0;
+`;
+
+const Navigation = Styled.nav`
+    padding: 25px 20px;
+`;
+
+const ListItem = Styled.li`
+    padding: 10px 5px;
+    font-size: 1.15rem;
+    &:hover {
+        background-color: #596ed9;
+    }
+`;
+
 const Sidebar = () => {
   return (
-    <aside>
-      <h2>Sidebar</h2>
-    </aside>
+    <Aside>
+      <Logo>
+        <h2>LOGO</h2>
+      </Logo>
+      <Navigation>
+        <NavigationList>
+          <ListItem>Home</ListItem>
+          <ListItem>Add Event</ListItem>
+          <ListItem>Options</ListItem>
+        </NavigationList>
+      </Navigation>
+    </Aside>
   );
 };
 
