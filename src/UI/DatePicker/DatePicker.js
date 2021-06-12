d682d339b3e8499de8b97bad6f3f247d861765aa
nesherson <nesad.omeragich27@gmail.com>
2021-06-12 14:51:31 +0200

    Implements date functionality

--- src/UI/DatePicker/DatePicker.js
+++ src/UI/DatePicker/DatePicker.js
@@ -5,20 +5,39 @@ const formatDate = (date) => {
   return new Date(date);
 };
 
-const _MS_PER_DAY = 1000 * 60 * 60 * 24;
-
-function dateDiffInDays(a, b) {
-  const utc1 = Date.UTC(a.getFullYear(), a.getMonth(), a.getDate());
-  const utc2 = Date.UTC(b.getFullYear(), b.getMonth(), b.getDate());
-
-  return Math.floor((utc2 - utc1) / _MS_PER_DAY);
+const formatSelectedDate = (date) => {
+  const updatedDate = new Date(date);
+  return new Date(updatedDate.getFullYear(), updatedDate.getMonth(), updatedDate.getDate(), 0, 0, 0, 0);
 }
 
-function toDateTime(secs) {
-  var t = new Date(Date.UTC(1970, 0, 1)); // Epoch
-  t.setUTCSeconds(secs);
-  return t;
-}
+function secondsToDhms(seconds) {
+  seconds = Number(seconds);
+  var d = Math.floor(seconds / (3600*24));
+  var h = Math.floor(seconds % (3600*24) / 3600);
+  var m = Math.floor(seconds % 3600 / 60);
+  var s = Math.floor(seconds % 60);
+  
+  // var dDisplay = d > 0 ? d + (d == 1 ? " day, " : " days, ") : "";
+  // var hDisplay = h > 0 ? h + (h == 1 ? " hour, " : " hours, ") : "";
+  // var mDisplay = m > 0 ? m + (m == 1 ? " minute, " : " minutes, ") : "";
+  // var sDisplay = s > 0 ? s + (s == 1 ? " second" : " seconds") : "";
+  // return dDisplay + hDisplay + mDisplay + sDisplay;
+  return {
+    days: d,
+    hours: h,
+    minutes: m,
+    seconds: s
+  };
+  }
+
+  const getTimeBetweenDates = (current, selected) => {
+    const currentSec = formatDate(current).getTime();
+    const selectedSec = formatSelectedDate(selected).getTime();
+  
+    const seconds = Math.abs((selectedSec - currentSec)/1000);
+
+    return secondsToDhms(seconds);
+  }
 
 const DatePicker = () => {
   const [date, setDate] = useState('2021-06-12');
@@ -29,17 +48,11 @@ const DatePicker = () => {
 
   const currentDate = Date.now();
 
-  console.log('current date --> ', formatDate(currentDate).getTime());
+ 
 
-  console.log('selected date --> ', formatDate(date).getTime());
+  console.log(getTimeBetweenDates(currentDate, date));
 
-  const minutes = Math.floor(
-    Math.abs(formatDate(date) - formatDate(currentDate)) / 1000 / 60
-  );
-  const days = dateDiffInDays(formatDate(currentDate), formatDate(date));
 
-  console.log('diff/days --> ', days);
-  console.log('diff/min --> ', minutes);
 
   return (
     <div>
