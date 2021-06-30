399241014c30756b709b8a83bb254dfc9b63e38b
unknown <nesad.omeragich27@gmail.com>
2021-06-30 03:34:21 +0200

    Code refactor

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-import Sidebar from './components/Sidebar/Sidebar';
 import Homepage from './components/Homepage/Homepage';
 
 function App() {
   return (
     <div>
-      <Sidebar />
       <Homepage />
     </div>
   );
